9c26d5b46633fa1a043068f5f38c6e9a2b066727
Nopdanai <aun_bds-st@rmutsb.ac.th>
2019-09-16 19:21:02 +0700

    Nopdanai

---
+++ WebApplication1/Controllers/HomeController.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+        public IActionResult plus(int id)
+        {
+            int i = id + 10;
+            ViewData["Plus"] = i;
+            return View();
+        }
+        public IActionResult multiply(int id)
+        {
+            int i = id * 10;
+            ViewData["multiply"] = i;
+            return View();
+        }
+        [Route("Minus/{a}/{b}")]
+        public IActionResult Minus(int a, int b)
+        {
+            int i = a - b;
+            ViewData["Minus"] = i;
+            return View();
+        }
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+        public IActionResult Contact()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult ContactPost(IFormCollection values)
+        {
+            ViewData["Name"] = values["name"];
+            ViewData["email"] = values["email"];
+            ViewData["ShowAlert"] = true;
+            return View("Index");
+        }
+
+        public IActionResult Contact2()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult ContactPost2(Contact model)
+        {
+            ViewData["Name"] = model.Name;
+            ViewData["email"] = model.Email;
+            ViewData["ShowAlert"] = true;
+            return View("Index");
+        }
+    }
+}
---
+++ WebApplication1/Models/Contact.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication1.Models
+{
+    public class Contact
+    {
+        
+        public string Name { get; set; }
+        [EmailAddress]
+        public string Email { get; set; }
+        public string Subject { get; set; }
+        public string Message { get; set; }
+        public string SendCopy { get; set; }
+    }
+}
